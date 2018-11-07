4b3d2c37cd10562728613ab6d60357a62155e53f
Brooke Gill <brooke.gill25@gmail.com>
2018-11-07 19:50:57 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -4,9 +4,19 @@ input.addEventListener('mouseover', function(e) {
     alert('Hello there!');
 });
 
-// Prevent numbers in input
 input.addEventListener('keydown', function(e) {
   if (e.which === 20) {
     alert('Caps lock is on!')
   }
 });
+
+const divs = document.querySelectorAll('div');
+ 
+function bubble(e) {
+  e.stopPropagation();
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+};
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+};
