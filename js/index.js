17136278d8b342a4e31c8e99696bd159eb4769ba
Brooke Gill <brooke.gill25@gmail.com>
2018-11-07 19:51:35 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,22 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+const input = document.querySelector('input');
+
+input.addEventListener('mouseover', function(e) {
+    alert('Hello there!');
+});
+
+input.addEventListener('keydown', function(e) {
+  if (e.which === 20) {
+    alert('Caps lock is on!')
+  }
+});
+
+const divs = document.querySelectorAll('div');
+
+function bubble(e) {
+  e.stopPropagation();
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+};
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+};
