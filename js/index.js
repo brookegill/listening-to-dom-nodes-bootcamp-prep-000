7b82339e361c68d16a0b6a68d190bcf0021a7920
Brooke Gill <brooke.gill25@gmail.com>
2018-11-07 19:51:12 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -11,12 +11,12 @@ input.addEventListener('keydown', function(e) {
 });
 
 const divs = document.querySelectorAll('div');
- 
+
 function bubble(e) {
   e.stopPropagation();
   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
 };
- 
+
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', bubble);
 };
